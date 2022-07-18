9786317ca5b1612be5edb029f9d48849a34e2233
Maciek Ziemba <maciej.ziemba@findewal.pl>
2022-07-18 09:12:16 +0200

    nowy build 18 lipca

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyCXi-6EC2tFh964tQBs8XewCVFkeESk4W4",
+    authDomain: "k2022-aad6b.firebaseapp.com",
+    databaseURL: "https://k2022-aad6b-default-rtdb.firebaseio.com",
+    projectId: "k2022-aad6b",
+    storageBucket: "k2022-aad6b.appspot.com",
+    messagingSenderId: "481029861583",
+    appId: "1:481029861583:web:bc342f0b31bbe564f287ba",
+    measurementId: "G-XG7C8E442L"
+  },
 };
